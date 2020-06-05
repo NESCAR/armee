1f7eb5e4c8b47f77d218a6f2f8134d43f8fced1a
Neyzoter <sonechaochao@gmail.com>
2020-06-05 11:04:31 +0800

    rm unused pack

--- app-ris/src/main/java/abc/ney/armee/appris/ApplicationBoot.java
+++ app-ris/src/main/java/abc/ney/armee/appris/ApplicationBoot.java
@@ -1,13 +1,9 @@
 package abc.ney.armee.appris;
 
-import abc.ney.armee.appris.config.ApolloConfiger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.security.sasl.SaslServer;
 
 /**
  * 应用
