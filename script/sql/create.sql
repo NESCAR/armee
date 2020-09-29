affafe0d27682e7363d731d22c498cadceb00bfc
neyzoter <sonechaochao@gmail.com>
2020-09-29 21:18:36 +0800

    update sql

--- script/sql/create.sql
+++ script/sql/create.sql
@@ -1,3 +1,5 @@
+DROP DATABASE tms;
+CREATE DATABASE tms;
 USE tms;
 
 DROP TABLE IF EXISTS `authority`;
@@ -59,15 +61,22 @@ CREATE TABLE `oauth_client_details` (
     `access_token_validity` int(11) DEFAULT NULL COMMENT '访问令牌有效时间值(单位:秒)',
     `refresh_token_validity` int(11) DEFAULT NULL COMMENT '更新令牌有效时间值(单位:秒)',
     `additional_information` varchar(255) DEFAULT NULL COMMENT '预留字段',
-    `autoapprove` varchar(255) DEFAULT NULL COMMENT '用户是否自动Approval操作'
+    `autoapprove` varchar(255) DEFAULT NULL COMMENT '用户是否自动Approval操作',
+    PRIMARY KEY (client_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /** 客户端信息 */
+# user -> $2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG
 INSERT INTO oauth_client_details VALUES('user_client','project_api',
                                         '$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2',
                                         'read,write', 'password,refresh_token', 'http://127.0.0.1',
                                         'ROLE_SUPER_ADMIN', 7200, 1800, NULL, 'true');
+# web_application_123456 -> $2a$10$.XShZVO1r92kcA4jaW66A.1eiO/G312rgXmZivQFAwKkn80ZlUT9i
+INSERT INTO oauth_client_details VALUES('web_application','project_api',
+                                        '$2a$10$.XShZVO1r92kcA4jaW66A.1eiO/G312rgXmZivQFAwKkn80ZlUT9i',
+                                        'read,write', 'password,refresh_token', 'http://127.0.0.1',
+                                        'ROLE_SUPER_ADMIN', 7200, 1800, NULL, 'true');
 SELECT * FROM `oauth_client_details`;
-UPDATE oauth_client_details SET authorities = 'ROLE_SUPER_ADMIN' where client_id = 'user_client';
+# UPDATE oauth_client_details SET authorities = 'ROLE_SUPER_ADMIN' where client_id = 'user_client';
 #DELETE FROM oauth_client_details where client_id = 'user_client';
 
 DROP TABLE IF EXISTS `oauth_access_token`;
@@ -88,24 +97,19 @@ CREATE TABLE `oauth_refresh_token` (
    `authentication` longblob COMMENT 'OAuth2Authentication.java对象序列化后的二进制数据'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-USE tms;
-
-DROP TABLE IF EXISTS `driver`;
-CREATE TABLE `driver` (
-  `gid` BIGINT UNSIGNED AUTO_INCREMENT COMMENT '驾驶员唯一id',
-  `user_name` VARCHAR(255) NOT NULL COMMENT '用户名',
-  `real_name` VARCHAR(255) NOT NULL COMMENT '用户真实姓名',
-  `no` VARCHAR(255) NOT NULL COMMENT '工号',
-  `psw` VARCHAR(255) NOT NULL COMMENT '用户密码',
-  `tel` VARCHAR(32) DEFAULT NULL COMMENT '手机号',
-  `role_id` BIGINT NOT NULL COMMENT '司机角色',
-  `email` VARCHAR(128) DEFAULT NULL COMMENT '邮箱',
-  `gmt_create` DATETIME(6) DEFAULT NULL COMMENT '创建时间',
-  `gmt_update` DATETIME(6) DEFAULT NULL COMMENT '更新时间',
-  CONSTRAINT driver_role_id_cons FOREIGN KEY (role_id) REFERENCES role(gid),
-  PRIMARY KEY (`gid`)
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE `staff` (
+         `gid` BIGINT UNSIGNED AUTO_INCREMENT COMMENT '唯一id',
+         `real_name` VARCHAR(255) NOT NULL COMMENT '真实姓名',
+         `no` VARCHAR(255) NOT NULL COMMENT '工号',
+         `position` VARCHAR(255) DEFAULT NULL COMMENT '职位',
+         `tel` VARCHAR(32) NOT NULL COMMENT '手机号',
+         `email` VARCHAR(128) DEFAULT NULL COMMENT '邮箱',
+         `gmt_create` DATETIME(6) DEFAULT NULL COMMENT '创建时间',
+         `gmt_update` DATETIME(6) DEFAULT NULL COMMENT '更新时间',
+         PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-DESC `driver`;
+DESC `staff`;
 
 DROP TABLE IF EXISTS `device`;
 CREATE TABLE `device` (
@@ -120,77 +124,17 @@ CREATE TABLE `device` (
   `driver_gid` BIGINT UNSIGNED DEFAULT NULL  COMMENT '驾驶员唯一id',
   `gmt_create` DATETIME(6) DEFAULT NULL COMMENT '创建时间',
   `gmt_update` DATETIME(6) DEFAULT NULL COMMENT '更改时间',
-  CONSTRAINT driver_gid_cons FOREIGN KEY (driver_gid) REFERENCES driver(gid),
+  CONSTRAINT driver_gid_cons FOREIGN KEY (driver_gid) REFERENCES staff(gid),
   PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 DESC `device`;
 
-DROP TABLE IF EXISTS `manager`;
-CREATE TABLE `manager` (
-   `gid` BIGINT UNSIGNED AUTO_INCREMENT COMMENT '管理员唯一id',
-   `real_name` VARCHAR(255) NOT NULL COMMENT '真实姓名',
-   `user_name` VARCHAR(255) NOT NULL COMMENT '用户名',
-   `psw` VARCHAR(255) NOT NULL COMMENT '密码',
-   `role_id` BIGINT NOT NULL COMMENT '角色id',
-   `no` VARCHAR(255) NOT NULL COMMENT '工号',
-   `oauth_id` VARCHAR(255) NOT NULL COMMENT '密码',
-   `tel` VARCHAR(32) NOT NULL COMMENT '手机号',
-   `email` VARCHAR(128) DEFAULT NULL COMMENT '邮箱',
-   `gmt_create` DATETIME(6) DEFAULT NULL COMMENT '创建时间',
-   `gmt_update` DATETIME(6) DEFAULT NULL COMMENT '更新时间',
-   CONSTRAINT role_id FOREIGN KEY (role_id) REFERENCES role(gid),
-   PRIMARY KEY (`gid`)
+
+DROP TABLE IF EXISTS `staff_oauth_client_details`;
+CREATE TABLE `oauth_client_details_staff` (
+    `client_id` VARCHAR(255) COMMENT '客户端id',
+    `staff_id` BIGINT UNSIGNED COMMENT '员工id',
+    CONSTRAINT client_id_cons FOREIGN KEY (client_id) REFERENCES oauth_client_details(client_id),
+    CONSTRAINT staff_id_cons FOREIGN KEY (staff_id) REFERENCES staff(gid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-DESC `manager`;
-
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-    `gid` BIGINT UNSIGNED COMMENT '用户角色id',
-    `info` VARCHAR(255) NOT NULL COMMENT '角色说明',
-    `gmt_create` DATETIME(6) DEFAULT NULL COMMENT '创建时间',
-    `gmt_update` DATETIME(6) DEFAULT NULL COMMENT '更改时间',
-    PRIMARY KEY (`gid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `role` (gid, info, gmt_create, gmt_update)
-VALUES (0, '超级管理员', NOW(), NOW());
-INSERT INTO `role` (gid, info, gmt_create, gmt_update)
-VALUES (1, '普通管理员', NOW(), NOW());
-INSERT INTO `role` (gid, info, gmt_create, gmt_update)
-VALUES (2, '普通员工', NOW(), NOW());
-SELECT * FROM `role`;
-
-DROP TABLE IF EXISTS `permission`;
-CREATE TABLE `permission` (
-  `gid` BIGINT UNSIGNED COMMENT '权限id',
-  `url` VARCHAR(128) NOT NULL COMMENT '权限url模式',
-  `info` VARCHAR(255) NOT NULL COMMENT '权限信息',
-  `gmt_create` DATETIME(6) DEFAULT NULL COMMENT '创建时间',
-  `gmt_update` DATETIME(6) DEFAULT NULL COMMENT '更改时间',
-  PRIMARY KEY (`gid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `permission` (gid, url, info, gmt_create, gmt_update)
-VALUES (0, 'carInfo/**', '汽车信息', NOW(), NOW());
-INSERT INTO `permission` (gid, url, info, gmt_create, gmt_update)
-VALUES (1, 'carControl/**', '汽车控制', NOW(), NOW());
-INSERT INTO `permission` (gid, url, info, gmt_create, gmt_update)
-VALUES (2, 'userManage/manager/**', '普通管理员管理（超级管理员拥有的权限）', NOW(), NOW());
-INSERT INTO `permission` (gid, url, info, gmt_create, gmt_update)
-VALUES (3, 'userManage/commonStaff/**', '普通员工管理（超级管理员和普通管理员均拥有的权限）', NOW(), NOW());
-
-DROP TABLE IF EXISTS `role_permission`;
-CREATE TABLE `role_permission` (
-   `role_id` BIGINT UNSIGNED COMMENT '角色id，对应到role表到gid',
-   `permission_id` BIGINT UNSIGNED COMMENT '权限id',
-   CONSTRAINT role_cons FOREIGN KEY (role_id) REFERENCES role(gid),
-   CONSTRAINT permission_cons FOREIGN KEY (permission_id) REFERENCES permission(gid)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `role_permission` (role_id, permission_id)
-VALUES (0, 0), (0, 1), (0, 2), (0, 3);
-INSERT INTO `role_permission` (role_id, permission_id)
-VALUES (1, 0), (1, 2), (1, 3);
-SELECT * FROM `role_permission`;
-
-USE tms;
-SHOW tables;
-
-select * from authority;
+DESC oauth_client_details_staff;
